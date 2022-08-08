3107b00ae0830936e0eb93702f306181fa4f8a0b
Yapoluchelovek <107258282+Yapoluchelovek@users.noreply.github.com>
2022-08-08 13:25:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let name = "Алексей";
+let age = 18;
+
+let sentense = `Привет меня зовут ${name}, мне ${name}, хотел бы поделится с вами таким мини калькулятором`
+console.log(sentense);
+
+let firstNumber = 2;
+let secondNumber = 2;
+let answer = 4;
+
+let miniCalc = `${firstNumber} + ${secondNumber} = ${answer}`
+
+console.log(miniCalc);
